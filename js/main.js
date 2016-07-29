e9c538b43c8e3cc5566f2174a457df5d46b1abaa
rev005 <revanth2009@gmail.com>
2016-07-29 17:55:24 -0500

    logo click functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
   console.log('testing');
+  $('.initial').on('click',function(e){
+  	$('.activePill').removeClass('activePill');
+  });
+
   $('#menu1, #menu2, #menu3, #menu4').on('click',function(e){
   	$('.activePill').removeClass('activePill');
   	$(this).addClass('activePill');
